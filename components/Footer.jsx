a4c8da5e459e28b338f70972c1860705dd29fa63
ilya4 <ilya41473@gmail.com>
2022-10-27 18:14:23 +0300

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 Happy Corg All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ components/PetBanner.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { urlFor } from '../lib/client';
+
+const PetBanner = ({ petBanner }) => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">{petBanner.smallText}</p>
+        <h3>{petBanner.midText}</h3>
+        <h1>{petBanner.largeText1}</h1>
+        <img src={urlFor(petBanner.image)} alt="Pet" className="hero-banner-image" />
+
+        <div>
+          <Link href={`/product/${petBanner.product}`}>
+            <button type="button">{petBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{petBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PetBanner
